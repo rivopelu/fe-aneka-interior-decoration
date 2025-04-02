02b8496f9615dd77db19908f5507c90133d61acb
rivopelu <rivopelu12@gmail.com>
2025-04-03 05:12:04 +0700

    FEAT:setup

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,11 +1,3 @@
 export const ENDPOINT = {
-  LIST_KEYWORD: () => `/master-data/v1/keyword/list`,
   SIGN_IN: () => `/auth/v1/sign-in`,
-  LIST_TOPIC: () => `/master-data/v1/topic/list`,
-  CREATE_TOPIC: () => `/master-data/v1/topic/create`,
-  CREATE_KEYWORD: () => `/master-data/v1/keyword/create`,
-  CREATE_NEW_POST: () => `/post/v1/new`,
-  LIST_POST: () => `/post/v1/cms/list`,
-  DETAIL_POST: (id: string) => `/post/v1/cms/detail/${id}`,
-  PUBLISH_POST: (id: string) => `/post/v1/publish/${id}`,
 };
